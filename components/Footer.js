d809a0f3deca422a1bd184805803dffff8ea4ace
Nurul Jaelani <nuruljaelani96@gmail.com>
2021-08-22 09:53:03 +0700

    jancoooook

--- components/Footer.js
+++ components/Footer.js
@@ -5,35 +5,35 @@ const Footer = () => (
       <div className="grid grid-cols-4 gap-6 px-4 pt-16 pb-20">
         <div className="flex flex-col">
           <p className="font-semibold text-gray-700">Tokopaedi</p>
-          <div className="flex flex-col text-sm mt-4">
-            <p className="text-gray-500 mb-2">Lorem ipsum</p>
-            <p className="text-gray-500 mb-2">Lorem ipsum</p>
-            <p className="text-gray-500 mb-2">Lorem ipsum</p>
-            <p className="text-gray-500 mb-2">Lorem ipsum</p>
-            <p className="text-gray-500 mb-2">Lorem ipsum</p>
-            <p className="text-gray-500 mb-2">Lorem ipsum</p>
+          <div className="flex flex-col text-sm mt-2">
+            <p className="text-gray-500 mb-2">Tentang Tokopaedi</p>
+            <p className="text-gray-500 mb-2">Hak kekayaan intelektual</p>
+            <p className="text-gray-500 mb-2">Karir</p>
+            <p className="text-gray-500 mb-2">Blog</p>
+            <p className="text-gray-500 mb-2">Mitra blog</p>
+            <p className="text-gray-500 mb-2">Toko Point</p>
           </div>
         </div>
         <div className="flex flex-col">
-          <p className="font-semibold text-gray-700">Tokopaedi</p>
-          <div className="flex flex-col text-sm mt-4">
-            <p className="text-gray-500 mb-2">Lorem ipsum</p>
-            <p className="text-gray-500 mb-2">Lorem ipsum</p>
-            <p className="text-gray-500 mb-2">Lorem ipsum</p>
-            <p className="text-gray-500 mb-2">Lorem ipsum</p>
-            <p className="text-gray-500 mb-2">Lorem ipsum</p>
-            <p className="text-gray-500 mb-2">Lorem ipsum</p>
+          <p className="font-semibold text-gray-700">Beli</p>
+          <div className="flex flex-col text-sm mb-4 mt-2">
+            <p className="text-gray-500 mb-2">Tagihan & Topup</p>
+            <p className="text-gray-500 mb-2">Tukar tambah Handphone</p>
+            <p className="text-gray-500 mb-2">Tokopaedi COD</p>
+          </div>
+          <p className="font-semibold text-gray-700">Jual</p>
+          <div className="flex flex-col text-sm mt-2">
+            <p className="text-gray-500 mb-2">Pusat Edukasi Seller</p>
+            <p className="text-gray-500 mb-2">Mitra</p>
+            <p className="text-gray-500 mb-2">Daftar Official Store</p>
           </div>
         </div>
         <div className="flex flex-col">
-          <p className="font-semibold text-gray-700">Tokopaedi</p>
-          <div className="flex flex-col text-sm mt-4">
-            <p className="text-gray-500 mb-2">Lorem ipsum</p>
-            <p className="text-gray-500 mb-2">Lorem ipsum</p>
-            <p className="text-gray-500 mb-2">Lorem ipsum</p>
-            <p className="text-gray-500 mb-2">Lorem ipsum</p>
-            <p className="text-gray-500 mb-2">Lorem ipsum</p>
-            <p className="text-gray-500 mb-2">Lorem ipsum</p>
+          <p className="font-semibold text-gray-700">Bantuan dan Panduan</p>
+          <div className="flex flex-col text-sm mt-2">
+            <p className="text-gray-500 mb-2">Tokopaedi Care</p>
+            <p className="text-gray-500 mb-2">Syarat dan Ketentuan</p>
+            <p className="text-gray-500 mb-2">Kebujakan Privasi</p>
           </div>
         </div>
         <div className="flex flex-col">
